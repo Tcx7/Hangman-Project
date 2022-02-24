d53fa87a9b3034fb9ccef5c150514de59ab726f4
Tcx7 <98015500+Tcx7@users.noreply.github.com>
2022-02-23 21:44:58 -0500

    Initial commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+let words = ["banana", "apple", "orange"];
+console.log(words[Math.floor(Math.random() * words.length)]);
+
+const startBtn = document.getElementById("gameBtn");
+console.log(startBtn);
